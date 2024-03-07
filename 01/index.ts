a938d18a5945d2b959deb4f37a0c33357a6104c5
Juan Nicolas <juannicolasdev@gmail.com>
2024-03-07 14:31:18 -0300

    Exercicio 01 concluido

---
+++ 01/index.ts
@@ -0,0 +1,27 @@
+const fs = require('fs')
+
+function leitura(): void {
+    const arquivo: string = fs.readFileSync('../bd.json')
+
+    return console.log(JSON.parse(arquivo));
+}
+
+function escrever(): void {
+    const arquivo: string = fs.readFileSync('../bd.json')
+
+    const arquivoParse = JSON.parse(arquivo)
+
+    arquivoParse.push({
+        nome: 'Gabriel',
+        idade: 21
+    })
+
+    const arquivoStringify = JSON.stringify(arquivoParse)
+
+    console.log(arquivoStringify);
+
+    fs.writeFileSync('../bd.json', arquivoStringify)
+}
+
+leitura();
+escrever()
