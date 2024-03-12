57ae4295056cc65f12579733f787a6d722945c07
Juan Nicolas <juannicolasdev@gmail.com>
2024-03-12 12:19:09 -0300

    Exercicio 04 concluido

---
+++ 04/index.ts
@@ -0,0 +1,53 @@
+const fs = require("fs");
+
+type Endereco = {
+  cep: string;
+  rua: string;
+  complemento?: string;
+  bairro: string;
+  cidade: string;
+};
+
+type Usuario = {
+  nome: string;
+  email: string;
+  cpf: string;
+  profissao?: string;
+  endereco?: Endereco | null;
+};
+
+// function detalhar(cpf: string): void {
+//   const arquivo: string = fs.readFileSync("../bd.json");
+
+//   const arquivoParse = JSON.parse(arquivo);
+
+//   const usuario: Usuario = arquivoParse.find((pessoa: Usuario) => {
+//     return pessoa.cpf === cpf;
+//   });
+
+//   console.log(usuario);
+// }
+
+function excluir(cpf: string): void {
+  const arquivo: string = fs.readFileSync("../bd.json");
+
+  let arquivoParse = JSON.parse(arquivo);
+
+  const index: number = arquivoParse.findIndex((pessoa: Usuario) => {
+    return pessoa.cpf === cpf;
+  });
+
+  if (index === -1) {
+    return console.log('Não existe usuário com esse cpf.');
+  }
+  
+  const usuarioExcluido = arquivoParse.splice(index, 1)
+
+  const arquivoJson: string = JSON.stringify(arquivoParse);
+
+  fs.writeFileSync("../bd.json", arquivoJson);
+
+  return console.log(usuarioExcluido[0]);
+}
+
+excluir("00000000000");
